c5eee0b1b6878009a7b116eb55b1c0e16fa2c21b
85VNL <93180438+vanilla85DT@users.noreply.github.com>
2023-03-13 16:26:06 +0700

    Add files via upload

---
+++ Blinking_GOD.ino
@@ -0,0 +1,79 @@
+
+int ledPin =  13;    // LED connected to digital pin 13
+
+// The setup() method runs once, when the sketch starts
+void setup()   {
+  // initialize the digital pin as an output:
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop() method runs over and over again,
+void loop()
+{
+    Blinky(1000);
+
+    Blinky(250);
+}
+
+void Blinky(int time)
+{
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(time);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(time);                  // wait for a second
+}
+
+
+///////////////////////////////////////////////////////////////
+#define BUILDIN_LED 2
+#define ledPIN1 23
+#define ledPIN2 22
+// ----------------------------------------------------------------------------------
+void first_task(void *pvParameter){
+  /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+     muxed to GPIO on reset already, but some default to other
+     functions and need to be switched to GPIO. Consult the
+     Technical Reference for a list of pads and their default
+     functions.)
+  */
+  pinMode(ledPIN1,OUTPUT);
+  while(true) {
+    digitalWrite(ledPIN1, HIGH);
+    vTaskDelay(50 / portTICK_RATE_MS);
+    digitalWrite(ledPIN1, LOW);
+    vTaskDelay(50 / portTICK_RATE_MS);
+  }
+}
+// ----------------------------------------------------------------------------------
+void second_task(void *pvParameter){
+  /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+     muxed to GPIO on reset already, but some default to other
+     functions and need to be switched to GPIO. Consult the
+     Technical Reference for a list of pads and their default
+     functions.)
+  */
+  pinMode(ledPIN2,OUTPUT);
+  while(true) {
+    digitalWrite(ledPIN2, HIGH);
+    vTaskDelay(500 / portTICK_RATE_MS);
+    digitalWrite(ledPIN2, LOW);
+    vTaskDelay(500 / portTICK_RATE_MS);
+  }
+}
+void setup() {
+  
+  pinMode(BUILDIN_LED,OUTPUT);
+  // xTaskCreate( pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask)
+  //first thread blinky in 50ms
+  xTaskCreate(&first_task, "first_task", 1024, NULL, 5, NULL);
+  //second thread blinky in 500ms
+  xTaskCreate(&second_task,"second_task",1024, NULL, 5, NULL);
+  
+}
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(BUILDIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);              // wait for a second
+  digitalWrite(BUILDIN_LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(250);              // wait for a second
+}
